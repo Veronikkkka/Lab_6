9d3af4034e0d272e9081d26b90442b763fc647d4
Veronika Shevtsova <veronika.shevtsova@ucu.edu.ua>
2022-10-19 22:14:08 +0300

    2

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -3,4 +3,17 @@ package flower.store;
 public class FlowerPack {
     private Flower flower;
     private int quantity;
+
+    public FlowerPack(Flower flower_, int quantity_) {
+        flower = flower_;
+        quantity = quantity_;
+    }
+
+    public FlowerPack() {
+
+    }
+
+    public double getPrice(){
+        return flower.getPrice() * quantity;
+    }
 }
